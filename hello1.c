dff8a2cbddbd7f688ca173c9f4bcc4a37748ea32
GRyslan <45923857+GRyslan@users.noreply.github.com>
2020-12-15 02:20:01 +0200

    Add files via upload

---
+++ hello1.c
@@ -0,0 +1,47 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/printk.h>
+
+MODULE_AUTHOR("Holovenets Ruslan IV-83");
+MODULE_DESCRIPTION("Hello world (LAB5)");
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int amount = 1;
+module_param(amount, uint, S_IRUGO);
+MODULE_PARM_DESC(times, "Amount (Hello world)");
+
+static int __init hello_init(void)
+{
+  uint i = 0;
+
+  if(amount == 0)
+  {
+    printk(KERN_WARNING "Parametr=0");
+  } 
+  else if(amount >= 5 && amount <= 10) 
+  {
+    printk(KERN_WARNING "5<=Parametr<=10");
+  } 
+  else if(amount > 10)
+  {
+    printk(KERN_ERR "Parametr>=10");
+    return -EINVAL;
+  }
+
+  for(i=0; i<amount; i++)
+  {
+    printk(KERN_EMERG "Hello world\n");
+  }
+
+  printk(KERN_INFO "Amount: %d\n", amount);
+
+  return 0;
+}
+
+static void __exit hello_exit(void)
+{
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
